408b65999f7b4d914b3acc8f777aaec57d30662f
sheldar@wp.pl <sheldar@wp.pl>
2016-01-08 00:08:01 +0100

    Updated imageDropzone to use new function declaration

--- src/imageDropzone.js
+++ src/imageDropzone.js
@@ -2,41 +2,35 @@ const React = require('react');
 const Dropzone = require('react-dropzone');
 
 module.exports = React.createClass({
-    getInitialState: function () {
-        return {
-            files: []
-        };
+    getInitialState () {
+        return {files: []};
     },
 
-    handleDrop: function (files) {
-        this.setState({
-            files: files
-        });
+    handleDrop (files) {
+        this.setState({files: files});
     },
 
-    handleOpenClick: function () {
-        this.refs.dropzone.open();
-    },
-
-    render: function () {
+    render () {
         return (
             <div>
-                <Dropzone onDrop={this.handleDrop} ref="dropzone">
+                <Dropzone onDrop={this.handleDrop}>
                     <div>{'Try dropping some files here, or click to select files to upload.'}</div>
                 </Dropzone>
-                <button onClick = {this.handleOpenClick} type="button">
-                    {'Open Dropzone'}
-                </button>
-                {
-                    this.state.files.length > 0 ? (
+                {this.state.files.length > 0
+                    ? (
                         <div>
-                            <h2> {'Uploading'} {this.state.files.length} {'files...'} </h2>
+                            <h2>
+                                {'Uploading'}
+                                {this.state.files.length}
+                                {'files...'}
+                            </h2>
                             <div>
-                            {this.state.files.map((file) => <img key={1} src={file.preview}/>)}
+                                {this.state.files.map((file) => <img key={1} src={file.preview}/>)}
                             </div>
                         </div>
-                    ) : null
-                }
+                    )
+                    : null
+}
             </div>
         );
     }
