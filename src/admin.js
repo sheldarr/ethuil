064f1866ee3c58ffd687545331c477907f00e965
sheldar@wp.pl <sheldar@wp.pl>
2016-01-07 22:36:05 +0100

    Installed react-bootstrap

--- src/admin.js
+++ src/admin.js
@@ -1,9 +1,53 @@
 const React = require('react');
 
+const Col = require('react-bootstrap').Col;
+const Grid = require('react-bootstrap').Grid;
+const Row = require('react-bootstrap').Row;
+
+const Nav = require('react-bootstrap').Nav;
+const Navbar = require('react-bootstrap').Navbar;
+const NavItem = require('react-bootstrap').NavItem;
+
+const Glyphicon = require('react-bootstrap').Glyphicon;
+
+const Panel = require('react-bootstrap').Panel;
+
 module.exports = React.createClass({
     render () {
         return (
-            <div>{'Administator Tools'}</div>
+            <div>
+                <Navbar>
+                    <Navbar.Header>
+                        <Navbar.Brand>
+                            <a href="#">{'Administrator Panel'}</a>
+                        </Navbar.Brand>
+                    </Navbar.Header>
+                    <Nav>
+                        <NavItem eventKey={1} href="#">{'Playlist'}</NavItem>
+                    </Nav>
+                </Navbar>
+                <Grid>
+                    <Row>
+                        <Col xs={6}>
+                            <Panel header={<span><Glyphicon glyph="road" />{' Cars'}</span>}>
+                                {'Panel Content'}
+                            </Panel>
+                        </Col>
+                        <Col xs={6}>
+                            <Panel header={<span><Glyphicon glyph="picture" />{' Backgrounds'}</span>}>
+                                {'Panel Content'}
+                            </Panel>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col xs={12}>
+                            <Panel header={<span><Glyphicon glyph="music" />{' Playlist'}</span>}>
+                                {'Panel Content'}
+                            </Panel>
+                        </Col>
+                    </Row>
+                </Grid>
+            </div>
         );
     }
 });
--- src/playlist.js
+++ src/playlist.js
@@ -1,9 +1,38 @@
 const React = require('react');
 
+const Background = require('./background');
+
 module.exports = React.createClass({
+    getInitialState () {
+        return {
+            contentStyle: {
+                background: 'white',
+                boxShadow: '0 0 20px black',
+                marginLeft: '58%',
+                marginTop: '17%',
+                padding: '20px',
+                position: 'fixed',
+                width: '22%'
+            },
+            imageStyle: {
+                left: 0,
+                maxWidth: '100%',
+                position: 'fixed',
+                top: 0
+            }
+        };
+    },
+
     render () {
         return (
-            <div>{'Playlist'}</div>
+            <div>
+                <Background/>
+                <div style={this.state.contentStyle}>
+                    <p>{"Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus"}</p>
+                    <p>{"Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus"}</p>
+                    <p>{"Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus</p><p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus"}</p>
+                </div>
+            </div>
         );
     }
 });
