2e066adb3c215b3afe7a1d7aa67fd3bba8ea8300
sheldar@wp.pl <sheldar@wp.pl>
2016-01-16 12:32:27 +0100

    Static content is now served by server

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const path = require('path');
 
 const app = express();
 
+app.use('/public', express.static(__dirname + '/public'));
+
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.listen(3030, function () {
-    console.log('Example app listening on port 3030!');
+    console.log('Server is running on port: 3030');
 });
