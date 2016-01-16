57f9aa331cdbf8e3985f682cdb7f7668ff6f76c5
sheldar@wp.pl <sheldar@wp.pl>
2016-01-16 12:25:40 +0100

    Removed start script from npm scripts, added server.js file

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(3030, function () {
+    console.log('Example app listening on port 3030!');
+});
